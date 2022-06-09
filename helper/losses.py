12f08ebaf8e40fde4f676e46397bb04f0d13506d
canallee <jl3789@cornell.edu>
2022-06-09 02:12:22 -0500

    npair loss done

---
+++ helper/losses.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+ 
+
+def NPairLoss(model_emb, temp):
+    '''
+    return the n-pair loss for back prop
+    features:  
+        model output embedding, dimension [bsz, n_all, out_dim], 
+        where bsz is batchsize, n_all is anchor, pos, neg (n_all = 1+1+n_neg)
+        and out_dim is embedding dimension
+    temp:
+        temperature     
+    '''
+    # l2 normalize every embedding
+    features = F.normalize(model_emb, dim=-1, p=2)
+    # features_T is [bsz, outdim, n_all], for performing batch dot product
+    features_T = torch.transpose(features, 1, 2)
+    # anchor is the first embedding 
+    anchor = features[:,0]
+    # anchor is the first embedding 
+    anchor_dot_features = torch.bmm(anchor.unsqueeze(1), features_T)/temp 
+    # anchor_dot_features now [bsz, n_all], contains 
+    anchor_dot_features = anchor_dot_features.squeeze(1)
+    # deduct by max logits, which will be 1/temp since features are L2 normalized 
+    logits = anchor_dot_features - 1/temp
+    # the exp(z_i dot z_a) excludes the dot product between itself
+    # exp_logits is of size [bsz, 1+n_neg]
+    exp_logits = torch.exp(logits[:, 1:])
+    exp_logits_sum = exp_logits.sum(1) # size [bsz]
+    # positive examples dot product z_i dot z_p, and z_p is the second element
+    pos_logits = logits[:, 1]
+    log_prob = pos_logits - exp_logits_sum
+    # loss is averaged over negative log probability 
+    loss = - log_prob.mean()
+    return loss
+
+    
