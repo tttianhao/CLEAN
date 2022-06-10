71e1349658c950863904f1dc2e09ca4d9121fa59
Tianhao Yu <ythyth96@gmail.com>
2022-06-10 12:26:16 -0500

    minor update prc

---
+++ prc.sh
@@ -0,0 +1,10 @@
+python eval_randnk.py -tr uniref100/uniref100_train_split_0 -te uniref100/uniref100_test_split_0_curate -p 0.00001 -up 0.005 -s 100 -EP True
+python eval_randnk.py -tr uniref100/uniref100_train_split_1 -te uniref100/uniref100_test_split_1_curate -p 0.00001 -up 0.005 -s 100 -EP True
+python eval_randnk.py -tr uniref100/uniref100_train_split_2 -te uniref100/uniref100_test_split_2_curate -p 0.00001 -up 0.005 -s 100 -EP True
+python eval_randnk.py -tr uniref100/uniref100_train_split_3 -te uniref100/uniref100_test_split_3_curate -p 0.00001 -up 0.005 -s 100 -EP True
+python eval_randnk.py -tr uniref100/uniref100_train_split_4 -te uniref100/uniref100_test_split_4_curate -p 0.00001 -up 0.005 -s 100 -EP True
+python eval_randnk.py -tr uniref100/uniref100_train_split_0 -te uniref100/uniref100_test_split_0_curate -p 0.00001 -up 0.005 -s 100
+python eval_randnk.py -tr uniref100/uniref100_train_split_1 -te uniref100/uniref100_test_split_1_curate -p 0.00001 -up 0.005 -s 100
+python eval_randnk.py -tr uniref100/uniref100_train_split_2 -te uniref100/uniref100_test_split_2_curate -p 0.00001 -up 0.005 -s 100
+python eval_randnk.py -tr uniref100/uniref100_train_split_3 -te uniref100/uniref100_test_split_3_curate -p 0.00001 -up 0.005 -s 100
+python eval_randnk.py -tr uniref100/uniref100_train_split_4 -te uniref100/uniref100_test_split_4_curate -p 0.00001 -up 0.005 -s 100
