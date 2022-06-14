41d27eebf2b9f366655fe993dd20aaed0c6b5f3c
canallee <jl3789@cornell.edu>
2022-06-13 22:29:10 -0500

    subset done for uniref10

--- prc.sh
+++ prc.sh
@@ -1,27 +1,29 @@
 for i in 0 1 2 3 4 
 do
-    
-    python eval_randnk.py -tr uniref100/uniref100_train_split_${i} \
-        -te uniref100/uniref100_test_split_${i}_curate \
+    python eval_randnk.py \
+        -tr uniref10_train_split_${i} \
+        -te uniref10_test_split_${i}_curate \
         -p 0.00001 -up 0.005 -s 100 -EP True ; 
-    python eval_randnk.py -tr uniref100/uniref100_train_split_${i} \
-        -te uniref100/uniref100_test_split_${i}_curate \
+    python eval_randnk.py \
+        -n uniref10/uniref10_split${i}_final \
+        -tr uniref10_train_split_${i} \
+        -te uniref10_test_split_${i}_curate \
         -p 0.00001 -up 0.005 -s 100
- 
 done
 
 
 
+# for i in 0 1 2 3 4 
+# do
+#     python eval_randnk.py -tr uniref100/uniref100_train_split_${i} \
+#         -te uniref100/uniref100_test_split_${i}_curate \
+#         -p 0.00001 -up 0.005 -s 100 -EP True ; 
+#     python eval_randnk.py 
+#         -n uniref10/uniref10_split${i}_final \
+#         -tr uniref100/uniref100_train_split_${i} \
+#         -te uniref100/uniref100_test_split_${i}_curate \
+#         -p 0.00001 -up 0.005 -s 100
+# done
 
 
-
-python eval_randnk.py -tr uniref100/uniref100_train_split_0 -te uniref100/uniref100_test_split_0_curate -p 0.00001 -up 0.005 -s 100 -EP True
-python eval_randnk.py -tr uniref100/uniref100_train_split_1 -te uniref100/uniref100_test_split_1_curate -p 0.00001 -up 0.005 -s 100 -EP True
-python eval_randnk.py -tr uniref100/uniref100_train_split_2 -te uniref100/uniref100_test_split_2_curate -p 0.00001 -up 0.005 -s 100 -EP True
-python eval_randnk.py -tr uniref100/uniref100_train_split_3 -te uniref100/uniref100_test_split_3_curate -p 0.00001 -up 0.005 -s 100 -EP True
-python eval_randnk.py -tr uniref100/uniref100_train_split_4 -te uniref100/uniref100_test_split_4_curate -p 0.00001 -up 0.005 -s 100 -EP True
-python eval_randnk.py -tr uniref100/uniref100_train_split_0 -te uniref100/uniref100_test_split_0_curate -p 0.00001 -up 0.005 -s 100
-python eval_randnk.py -tr uniref100/uniref100_train_split_1 -te uniref100/uniref100_test_split_1_curate -p 0.00001 -up 0.005 -s 100
-python eval_randnk.py -tr uniref100/uniref100_train_split_2 -te uniref100/uniref100_test_split_2_curate -p 0.00001 -up 0.005 -s 100
-python eval_randnk.py -tr uniref100/uniref100_train_split_3 -te uniref100/uniref100_test_split_3_curate -p 0.00001 -up 0.005 -s 100
-python eval_randnk.py -tr uniref100/uniref100_train_split_4 -te uniref100/uniref100_test_split_4_curate -p 0.00001 -up 0.005 -s 100
+ 
