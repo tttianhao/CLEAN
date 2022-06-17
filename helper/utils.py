ee6d9f24fed391e417ecff131db1c1b9714a3947
canallee <jl3789@cornell.edu>
2022-06-16 23:28:52 -0500

    t-sne

--- helper/utils.py
+++ helper/utils.py
@@ -33,6 +33,23 @@ def get_ec_id_dict(csv_name: str) -> dict:
                     ec_id[ec].add(rows[0])
     return id_ec, ec_id
 
+def get_ec_id_dict_non_prom(csv_name: str) -> dict:
+    csv_file = open(csv_name)
+    csvreader = csv.reader(csv_file, delimiter='\t')
+    id_ec = {}
+    ec_id = {}
+
+    for i, rows in enumerate(csvreader):
+        if i > 0:
+            if len(rows[1].split(';')) == 1:
+                id_ec[rows[0]] = rows[1].split(';')
+                for ec in rows[1].split(';'):
+                    if ec not in ec_id.keys():
+                        ec_id[ec] = set()
+                        ec_id[ec].add(rows[0])
+                    else:
+                        ec_id[ec].add(rows[0])
+    return id_ec, ec_id
 
 def format_esm(a):
     if type(a) == dict:
--- prc.sh
+++ prc.sh
@@ -1,14 +1,14 @@
-for i in 0 1 2 3 4 
+for i in 3 4 
 do
     # python eval_randnk.py \
-    #     -tr uniref100/uniref100_train_split_${i} \
-    #     -te uniref100/uniref100_test_split_${i}_curate \
+    #     -tr uniref1/uniref1_train_split_${i} \
+    #     -te uniref1/uniref1_test_split_${i}_curate \
     #     -p 0.00001 -up 0.005 -s 100 -EP True ; 
     python eval_randnk.py \
-        -n uniref100/uniref10_split${i}_supcon \
-        -tr uniref100/uniref100_train_split_${i} \
-        -te uniref100/uniref100_test_split_${i}_curate \
-        -p 0.00001 -up 0.005 -s 100
+        -n uniref10/uniref10_split${i}_supcon \
+        -tr uniref10/uniref10_train_split_${i} \
+        -te uniref10/uniref10_test_split_${i}_curate \
+        -p 0.00001 -up 0.005 -s 100 -o 256
 done
  
 # ------------------ for promiscuous enzymes ------------------ #
@@ -22,4 +22,4 @@ done
 
 # python -u .\eval_randnk.py  -te uniref100_prom_split_test_curate  -tr uniref100_prom_split_train  -EP true  -p 0.0001 -up 0.05 -s 100
 # python -u ./train-supcon.py -t uniref10/uniref10_train_split_3 -n uniref10_split3_supcon > out/uniref10_split3_supcon.out
-python -u ./train-supcon.py -t uniref1/uniref1_train_split_3 -n uniref1_split3 >> uniref1_split3.out
+#python -u ./train-supcon.py -t uniref1/uniref1_train_split_3 -n uniref1_split3 >> uniref1_split3.out
