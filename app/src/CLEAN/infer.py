982c40c0fd3b964a1bee5f9d52c674624aecfdf7
Canal Li <71532627+canallee@users.noreply.github.com>
2023-04-20 12:05:38 -0400

    Fix CPU only use case as in issue #12

--- app/src/CLEAN/infer.py
+++ app/src/CLEAN/infer.py
@@ -25,12 +25,12 @@ def infer_pvalue(train_data, test_data, p_value = 1e-5, nk_random = 20,
     
     if pretrained:
         try:
-            checkpoint = torch.load('./data/pretrained/'+ train_data +'.pth')
+            checkpoint = torch.load('./data/pretrained/'+ train_data +'.pth', map_location=device)
         except FileNotFoundError as error:
             raise Exception('No pretrained weights for this training data')
     else:
         try:
-            checkpoint = torch.load('./data/model/'+ model_name +'.pth')
+            checkpoint = torch.load('./data/model/'+ model_name +'.pth', map_location=device)
         except FileNotFoundError as error:
             raise Exception('No model found!')
         
@@ -38,9 +38,9 @@ def infer_pvalue(train_data, test_data, p_value = 1e-5, nk_random = 20,
     model.eval()
     # load precomputed EC cluster center embeddings if possible
     if train_data == "split70":
-        emb_train = torch.load('./data/pretrained/70.pt')
+        emb_train = torch.load('./data/pretrained/70.pt', map_location=device)
     elif train_data == "split100":
-        emb_train = torch.load('./data/pretrained/100.pt')
+        emb_train = torch.load('./data/pretrained/100.pt', map_location=device)
     else:
         emb_train = model(esm_embedding(ec_id_dict_train, device, dtype))
         
@@ -92,7 +92,7 @@ def infer_maxsep(train_data, test_data, report_metrics = False,
             raise Exception('No pretrained weights for this training data')
     else:
         try:
-            checkpoint = torch.load('./data/model/'+ model_name +'.pth')
+            checkpoint = torch.load('./data/model/'+ model_name +'.pth', map_location=device)
         except FileNotFoundError as error:
             raise Exception('No model found!')
             
