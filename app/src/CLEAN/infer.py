0e92d8bc16298951088662ea4f7f1af5b7b61ed6
canallee <jl3789@cornell.edu>
2025-04-02 23:49:40 -0700

    update CLEAN to be able to inference on the fly

--- app/src/CLEAN/infer.py
+++ app/src/CLEAN/infer.py
@@ -125,3 +125,61 @@ def infer_maxsep(train_data, test_data, report_metrics = False,
             f'>>> precision: {pre:.3} | recall: {rec:.3}'
             f'| F1: {f1:.3} | AUC: {roc:.3} ')
         print('-' * 75)
+
+
+## functions for inference on the fly (without saving esm embeddings)
+
+def infer_maxsep(train_data, test_data, report_metrics = False, 
+                 pretrained=True, model_name=None, gmm = None):
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+    dtype = torch.float32
+    id_ec_train, ec_id_dict_train = get_ec_id_dict('./data/' + train_data + '.csv')
+    id_ec_test, _ = get_ec_id_dict('./data/' + test_data + '.csv')
+    # load checkpoints
+    # NOTE: change this to LayerNormNet(512, 256, device, dtype) 
+    # and rebuild with [python build.py install]
+    # if inferencing on model trained with supconH loss
+    model = LayerNormNet(512, 128, device, dtype)
+    
+    if pretrained:
+        try:
+            checkpoint = torch.load('./data/pretrained/'+ train_data +'.pth', map_location=device)
+        except FileNotFoundError as error:
+            raise Exception('No pretrained weights for this training data')
+    else:
+        try:
+            checkpoint = torch.load('./data/model/'+ model_name +'.pth', map_location=device)
+        except FileNotFoundError as error:
+            raise Exception('No model found!')
+            
+    model.load_state_dict(checkpoint)
+    model.eval()
+    # load precomputed EC cluster center embeddings if possible
+    if train_data == "split70":
+        emb_train = torch.load('./data/pretrained/70.pt', map_location=device)
+    elif train_data == "split100":
+        emb_train = torch.load('./data/pretrained/100.pt', map_location=device)
+    else:
+        emb_train = model(esm_embedding(ec_id_dict_train, device, dtype))
+        
+    emb_test = model_embedding_test(id_ec_test, model, device, dtype)
+    eval_dist = get_dist_map_test(emb_train, emb_test, ec_id_dict_train, id_ec_test, device, dtype)
+    seed_everything()
+    eval_df = pd.DataFrame.from_dict(eval_dist)
+    ensure_dirs("./results")
+    out_filename = "results/" +  test_data
+    write_max_sep_choices(eval_df, out_filename, gmm=gmm)
+    if report_metrics:
+        pred_label = get_pred_labels(out_filename, pred_type='_maxsep')
+        pred_probs = get_pred_probs(out_filename, pred_type='_maxsep')
+        true_label, all_label = get_true_labels('./data/' + test_data)
+        pre, rec, f1, roc, acc = get_eval_metrics(
+            pred_label, pred_probs, true_label, all_label)
+        print("############ EC calling results using maximum separation ############")
+        print('-' * 75)
+        print(f'>>> total samples: {len(true_label)} | total ec: {len(all_label)} \n'
+            f'>>> precision: {pre:.3} | recall: {rec:.3}'
+            f'| F1: {f1:.3} | AUC: {roc:.3} ')
+        print('-' * 75)
+
